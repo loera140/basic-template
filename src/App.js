87c417111425aee26053b9e32129960e48e9265a
Anthony Loera <anthony.loera@Anthonys-MacBook-Pro.local>
2023-10-05 21:56:11 -0700

    Added data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import data from "./data.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {data.map((item) => (
+          <li key={item.id}>
+            <h1>{item.title}</h1>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
